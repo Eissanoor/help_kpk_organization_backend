12cb5ac49966e7e7821fb3785e8f55221ef3ce94
Eissanoor <eissaanoor@gmail.com>
2025-01-06 12:27:28 +0500

    Enhance CORS configuration in app.js to support preflight requests
    - Added support for preflight requests by including an options route for all origins.
    - Improved CORS middleware setup to enhance flexibility in handling cross-origin requests.
    
    These changes improve the application's ability to handle CORS preflight requests, enhancing compatibility with various client applications.

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.options("*", cors());
 app.use(
   helmet({
     crossOriginResourcePolicy: { policy: "cross-origin" },
