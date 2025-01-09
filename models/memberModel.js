c7ec6a86024ad92e9b2e36d5f332c4c10bf4064c
Eissanoor <eissaanoor@gmail.com>
2025-01-09 18:40:01 +0500

    Add member model to define schema for family members
    - Introduced a new `memberModel` to manage family member data, including fields for personal details, relationships, and educational information.
    - Implemented validation for specific fields such as `relation`, `gender`, and `maritalStatus` to ensure data integrity.
    - Added support for timestamps to track member record creation and updates.
    
    These changes establish a structured approach to handle family member information within the application.

---
+++ models/memberModel.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const memberSchema = new Schema({
+userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+},
+productIds: [{
+  type: mongoose.Schema.Types.ObjectId,
+  ref: "Product",
+}],
+ childName: String,
+ cnicNo: String,
+ relation: {
+    type: String,
+    required: true,
+    enum: ['father', 'husband', 'other']
+   },
+   relationCnic: String,
+   dateOfBirth: String,
+   gender: {type: String, enum: ['male', 'female']},
+   maritalStatus: {type: String, enum: ['single', 'married', 'divorced', 'widowed']},
+   nationality: String,
+   tehsil: String,
+   district: String,
+   religion: {type: String, enum: ['muslim', 'non-muslim']},
+   bloodGroup: {type: String, enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']},
+   qualification: String,
+   profession: String,
+   currentAddress: String,
+   permanentAddress: String,
+   contactNumber: String,
+   contactNumber2: String,
+   TypeOfAccommodation: {type: String, enum: ['rented', 'owned', 'living with other']},
+   noOfDependents: Number,
+   noOfChildren: Number,
+   noOfChildrenMale: Number,
+   noOfChildrenFemale: Number,
+   noOfChildrenInSchool: Number,
+   nameOfSchoolChildren: [{
+        name: String,
+        class: String,
+        age: String,
+        SchoolName: String
+    }],
+   addictiveDrugs: {type: String, enum: ['yes', 'no']},
+   addictiveDrugsDescription: String,
+   anyDisability: {type: String, enum: ['yes', 'no']},
+   anyDisabilityDescription: String,
+   politicalAffiliation: {type: String, enum: ['yes', 'no']},
+   politicalAffiliationDescription: String,
+   NGO: {type: String, enum: ['yes', 'no']},
+   NGODescription: String,
+
+}, {
+    timestamps: true
+});
+
+
+
+module.exports =  mongoose.model("Member", memberSchema)
+    
